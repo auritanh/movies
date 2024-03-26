17608d277d0c9ca339b2ee173b7b56519d7d0a40
auritanh <auritanurh136@gmail.com>
2024-03-26 12:04:43 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -19,38 +22,34 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
-    count = db.bucket.count_documents({})
-    num = count + 1
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive,headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[name="description"]')
+    image = og_image['content']
+    title = og_title['content']
+    desc = og_description['content']
     doc = {
-        'num': num,
-        'bucket': bucket_receive,
-        'done': 0
+        'image':image,
+        'title':title,
+        'description':desc,
+        'star':star_receive,
+        'comment':comment_receive
     }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg': 'data saved!'})
+    db.movies.insert_one(doc)
+    return jsonify({'msg':'POST request!'})
 
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form['num_give']
-    db.bucket.update_one(
-        {'num': int(num_receive)},
-        {'$set': {'done': 1}}
-    )
-    return jsonify({'msg': 'update done!'})
-
-@app.route("/bucket/delete", methods=["POST"])
-def bucket_delete():
-    num_receive = request.form['num_give']
-    db.bucket.delete_one({'num': int(num_receive)})
-    return jsonify({'msg': 'delete done!'})
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    buckets_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': buckets_list})
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movie_list = list(db.movies.find({}, {'_id': False}))
+    return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
